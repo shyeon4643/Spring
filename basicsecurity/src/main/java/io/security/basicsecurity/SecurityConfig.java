26939a7f7ebcfef090eaad591ad06968126c5e58
Seo Songhyeon <songhyeon4643@gmail.com>
2023-08-01 00:56:32 +0900

    필터 초기화와 다중 보안 설정

--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.security.basicsecurity;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,62 +30,32 @@ import static org.springframework.security.authorization.AuthorityReactiveAuthor
 * 웹 보안 활성화 시킴
  */
 @EnableWebSecurity
+@Order(0) //Order(1)이면 SecurityConfig2가 먼저 실행돼서 /admin에 인증 없이 접근 가능하다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
-        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS");
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN"); // 이렇게 써주면 admin사용자는 user와 sys가 접근 가능한 url에 접근을 못한다.
-        //auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN","USER","SYS"); // 이렇게 써주어야지 admin이 user와 sys가 접근 가능한 url에 접근 가능하다
-    }
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/admin/**") // 내부적으로 AntPathRequestMatcher 가 생성된다.
+                .authorizeRequests()// 인증 요청
+                .anyRequest().authenticated() // 인증 받아야함
+                .and()
+                .httpBasic();
 
+    }
+}
+
+@Configuration(proxyBeanMethods = false)
+@Order(1)
+class SecurityConfig2 extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http //인가 정책 설정
-                .authorizeHttpRequests() //요청에 대한 보안 검사 실행
-                .antMatchers("/login").permitAll()
-                .antMatchers("/user").hasRole("USER")
-                .antMatchers("/admin/pay").hasRole("ADMIN")
-                .antMatchers("/admin/**").hasAnyRole("ADMIN","SYS")
-                //.antMatchers("/admin/pay").hasRole("ADMIN") // /admin/pay에는 admin만 접근 가능한데 좁은 범위가 넓은 범위보다 뒤에 있어서 sys도 접근 가능하게 된다.
-                .anyRequest().authenticated(); //어떠한 요청에도 인증을 받도록 설정
         http
-                .formLogin() // formLogin 인증 방식을 사용하도록 설정
-                .successHandler(new AuthenticationSuccessHandler() { // 로그인 성공 시 핸들러
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        RequestCache requestCache = new HttpSessionRequestCache();
-                        // 세션에서 이미 저장되어 있는 이전 요청 정보를 추출한다
-                        SavedRequest savedRequest = requestCache.getRequest(request,response);
-                        String redirectUrl = savedRequest.getRedirectUrl();
-
-                        // 그 이전 요청 위치로 이동한다
-                        response.sendRedirect(redirectUrl);
-                    }
-                })
-        ;
-        http
-                .exceptionHandling()
-                .authenticationEntryPoint(new AuthenticationEntryPoint() { //인증 실패 시 처리
-                    @Override
-                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-                        /*
-                        AuthenticationEntryPoint를 직접 구현하게 되면
-                        우리가 만든 로그인 페이지로 이동하게 된다.
-                        스프링 시큐리티가 제공하는 로그인 페이지가 아니다.
-                         */
-                        response.sendRedirect("/login");
-                    }
-                })
-                .accessDeniedHandler(new AccessDeniedHandler() { //인가 실패 시 처리
-                    @Override
-                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-                        response.sendRedirect("/denied");
-                    }
-                })
-        ;
-
+                // 딱히 설정을 안해서 AnyRequestMatcher 가 생성된다.
+                .authorizeRequests()
+                .anyRequest().permitAll() //인증 받지 않아도 접근 가능
+                .and()
+                .formLogin(); //인증 방식은 form 로그인 방식
     }
 }
