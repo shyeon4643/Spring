3e1f8e97a875df7ee9ba0a2df0a6fe09ed058c0e
Seo Songhyeon <songhyeon4643@gmail.com>
2023-07-31 18:23:25 +0900

    사용자 정의 보안 기능 구현

---
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -0,0 +1,24 @@
+package io.security.basicsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+/*
+* WebSecurityConfiguration.class, SpringWebMvcImportSelector.class, OAuth2ImportSelector.class,HttpSecurityConfiguration.class 설정 클래스 임포트함
+* 웹 보안 활성화 시킴
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http //인가 정책 설정
+                .authorizeHttpRequests() //요청에 대한 보안 검사 실행
+                .anyRequest().authenticated(); //어떠한 요청에도 인증을 받도록 설정
+        http
+                .formLogin() // formLogin 인증 방식을 사용하도록 설정
+                ;
+    }
+}
