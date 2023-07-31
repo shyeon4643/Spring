9acf6bd987ce6c131a68f14377d91b01f2f25765
Seo Songhyeon <songhyeon4643@gmail.com>
2023-07-31 18:41:36 +0900

    Form Login 인증

--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -4,6 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 /*
@@ -19,6 +28,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated(); //어떠한 요청에도 인증을 받도록 설정
         http
                 .formLogin() // formLogin 인증 방식을 사용하도록 설정
+              //  .loginPage("/loginPage") // 사용자 정의 로그인 페이지
+                .defaultSuccessUrl("/") //로그인 성공 후 이동 페이지
+                .failureUrl("/login") //로그인 실패 후 이동 페이지
+                .usernameParameter("userId") //아이디 파라미터명 설정
+                .passwordParameter("passwd") //패스워드 파라미터명 설정
+                .loginProcessingUrl("/login_proc") //로그인 form action url
+                .successHandler(new AuthenticationSuccessHandler() { // 로그인 성공 후 핸들러
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("authentication : " + authentication.getName());
+                        response.sendRedirect("/");
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler(){ //로그인 실패 후 핸들러
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        System.out.println("exception : "+exception.getMessage());
+                        response.sendRedirect("/login");
+                    }
+                })
+                .permitAll() //login 페이지는 모두가 접근하도록 설정
                 ;
     }
 }
