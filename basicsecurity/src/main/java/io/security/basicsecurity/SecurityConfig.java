96f40f22c58f40d5f42e4d3a04f431545da46c7f
Seo Songhyeon <songhyeon4643@gmail.com>
2023-07-31 22:05:37 +0900

    권한 설정과 표현식

--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,20 +1,13 @@
 package io.security.basicsecurity;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
+import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasRole;
 
 @Configuration
 /*
@@ -24,21 +17,26 @@ import java.io.IOException;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN"); // 이렇게 써주면 admin사용자는 user와 sys가 접근 가능한 url에 접근을 못한다.
+        //auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN","USER","SYS"); // 이렇게 써주어야지 admin이 user와 sys가 접근 가능한 url에 접근 가능하다
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http //인가 정책 설정
                 .authorizeHttpRequests() //요청에 대한 보안 검사 실행
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").hasAnyRole("ADMIN","SYS")
+                //.antMatchers("/admin/pay").hasRole("ADMIN") // /admin/pay에는 admin만 접근 가능한데 좁은 범위가 넓은 범위보다 뒤에 있어서 sys도 접근 가능하게 된다.
                 .anyRequest().authenticated(); //어떠한 요청에도 인증을 받도록 설정
         http
                 .formLogin(); // formLogin 인증 방식을 사용하도록 설정
-        http
-                .sessionManagement()
-                .sessionFixation().newSession() // 사용자가 인증 성공하면 새로운 세션이 생성됨, 이전 세션에 사용하던 세션정보를 사용할 수 없음
-                //.sessionFixation().changeSessionId() // 사용자가 인증 성공하면, 기존 사용자의 세션에 sessionId만 변경됨, 3.1 이상
-                //.sessionFixation().migrateSession() // 사용자가 인증 성공하면, 기존 사용자의 세션에 sessionId만 변경됨, 3.1 이하
-                //.sessionFixation().none() //세션 고정 보호를 하지 않음
-        ;
+
     }
 }
