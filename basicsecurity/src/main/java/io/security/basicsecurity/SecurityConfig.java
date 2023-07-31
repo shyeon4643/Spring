943d0cc708de7168448d0f0868a21c8606780c29
Seo Songhyeon <songhyeon4643@gmail.com>
2023-07-31 20:52:54 +0900

    동시 세션 제어

--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -32,31 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeHttpRequests() //요청에 대한 보안 검사 실행
                 .anyRequest().authenticated(); //어떠한 요청에도 인증을 받도록 설정
         http
-                .formLogin() // formLogin 인증 방식을 사용하도록 설정
-                .defaultSuccessUrl("/"); //로그인 성공 후 이동 페이지
+                .formLogin(); // formLogin 인증 방식을 사용하도록 설정
         http
-                .logout() // 로그아웃 처리
-                .logoutUrl("/logout") //로그아웃 처리 url
-                .logoutSuccessUrl("/login") // 로그아웃 성공 후 이동페이지
-                .addLogoutHandler(new LogoutHandler() { // 로그아웃 핸들러
-                    @Override
-                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-                        HttpSession session = request.getSession();
-                        session.invalidate();
-                    }
-                })
-                .logoutSuccessHandler(new LogoutSuccessHandler() { // 로그아웃 성공 후 핸들러
-                    @Override
-                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        response.sendRedirect("/login");
-                    }
-                })
-                //로그인 상태로 sessionId 삭제하면 로그인 페이지로 가서 다시 인증해야하지만, remember-me 값을 가지고 있으면 sessionId가 없어도 다시 인증하지 않아도 됨
-                .and()
-                .rememberMe()//rememberme 기능 설정
-                .rememberMeParameter("remember") // 기본 파라미터명은 remember-me
-                .tokenValiditySeconds(3600) // Default는 14일
-                .userDetailsService(userDetailsService) //시스템에 있는 사용자 계정 조회, remeberme할 때 반드시 필요
+                .sessionManagement()
+                .maximumSessions(1) //최대 허용 가능 세션 수, -1 : 무제한 로그인 세션 허용
+                .maxSessionsPreventsLogin(true) // 동시 로그인 차단함, false : 기존 세션 만료(default)
                 ;
     }
 }
