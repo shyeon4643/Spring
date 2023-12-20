655a3625eed1d3a8787d551333566028fb20d7ec
shyeon4643 <songhyeon4643@gmail.com>
2023-12-20 12:15:32 +0900

    예외 상황 테스트 추가

--- TDD/src/test/java/com/tdd/tdd/chap03/ExpiryDateCalculator.java
+++ TDD/src/test/java/com/tdd/tdd/chap03/ExpiryDateCalculator.java
@@ -1,14 +1,20 @@
 package com.tdd.tdd.chap03;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
         int addedMonths = payData.getPayAmount()/10_000;
         if(payData.getFirstBillingDate()!=null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
             if(payData.getFirstBillingDate().getDayOfMonth() !=
             candidateExp.getDayOfMonth()){
+                if(YearMonth.from(candidateExp).lengthOfMonth()<
+                        payData.getFirstBillingDate().getDayOfMonth()) {
+                return candidateExp.withDayOfMonth(
+                        YearMonth.from(candidateExp).lengthOfMonth());
+                }
                 return candidateExp.withDayOfMonth(
                         payData.getFirstBillingDate().getDayOfMonth());
             }
